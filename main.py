0470694ede961c053124f65bd38df7be6d86d8ce
aggarwalnaman <aggarwalnaman78@gmail.com>
2020-04-08 14:22:36 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import speech_recognition as sr
+AudFile=("male.wav")
+
+r=sr.Recognizer()
+
+with sr.AudioFile(AudFile)  as audi:
+    audio=r.record(audi)
+
+try:
+    print("It contains : "+r.recognize_google(audio))
+except sr.UnknownValueError:
+    print("Can not understand audio")
+except sr.RequestError:
+    print("Could not found result")
+
